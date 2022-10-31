1ade3087cf25ca3a91a978aa27cbc40914c8325e
Aziz Tri S <74166387+Aztris@users.noreply.github.com>
2022-10-31 16:50:57 +0700

    Add files via upload

---
+++ BengkelMotor/src/bengkelmotor/BengkelMotorSwitchCase.java
@@ -0,0 +1,101 @@
+import java.util.Scanner;
+
+public class BengkelMotorSwitchCase {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        int jenis;
+        System.out.println("Jenis Motor :");
+        System.out.println("1. Matic");
+        System.out.println("2. Bebek");
+        System.out.println("3. Manual");
+        System.out.print("Pilih jenis motor : ");
+        jenis=input.nextInt();
+        
+        System.out.print("Masukkan Tahun : ");
+        int tahun = input.nextInt();
+        
+        System.out.println("Jenis oli :");
+        System.out.println("1. Top 1");
+        System.out.println("2. Top 2");
+        System.out.println("3. Top 3");
+        System.out.print("Masukkan Jenis Oli : ");
+        int oli = input.nextInt();
+        
+        int harga_oli;
+        String hadiah="-";
+        switch (oli) {
+            case 1:
+                harga_oli=15000;
+                if(jenis==1){
+                    harga_oli=0;
+                    hadiah = "1 oli jenis Top 1";
+                }
+                break;
+            case 2:
+                harga_oli=10000;
+                break;
+            case 3:
+                harga_oli = 5000;
+                break;
+            default:
+                harga_oli = 0;
+        }
+        
+        int service=0,potongan=0,diskon=0, biaya=0;
+        String motor="-";
+        switch (jenis) {
+            case 1:
+                motor = "Matic";
+                service = 25000;
+                
+                biaya = service + harga_oli;
+                if(tahun<2010){
+                    diskon = 10;
+                    potongan = biaya * diskon / 100;
+                }
+                break;
+            case 2:
+                motor="Bebek";
+                service = 20000;
+                
+                biaya = service + harga_oli;
+                if(tahun<2010){
+                    diskon = 5;
+                    potongan = biaya * diskon / 100;
+                }
+                break;
+            case 3:
+                motor = "Manual";
+                service = 30000;
+                
+                biaya = service + harga_oli;
+                if(tahun<2010){
+                    diskon = 5;
+                    potongan = biaya * diskon / 100;
+                }
+                break;
+            default:
+                System.exit(0);
+        }
+        
+        
+        int total = biaya - potongan;
+        
+        System.out.println("");
+        System.out.println("===============================");
+        System.out.println("Jenis Motor : "+motor);
+        System.out.println("Tahun : "+tahun);
+        System.out.println("Hadiah : "+hadiah);
+        System.out.println("Diskon : "+diskon+"%");
+        System.out.println("Potongan : "+potongan);
+        System.out.println("TOTAL : "+total);
+        System.out.println("");
+        System.out.print("Bayar : "); int bayar = input.nextInt();
+        int kembalian = bayar - total;
+        System.out.println("Kembali : "+kembalian);
+        System.out.println("===============================");
+        
+    }
+}
